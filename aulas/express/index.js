da616508c81c1c07e9033e5e03c2390d5fefd52e
Alekssandher <alekssandher1@hotmail.com>
2024-10-07 16:55:38 -0300

    updates

---
+++ aulas/express/index.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/contato", (req, res) => {
+    res.send("contato p")
+})
+
+app.listen(port, () => {
+    console.log(`Servidor https://localhost:${port}`)
+})
