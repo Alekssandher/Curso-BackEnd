17ae494c599553fadf282be24cfef4959e058f8d
Alekssandher <alekssandher1@hotmail.com>
2024-10-07 08:42:57 -0300

    updates

--- aulas/aul4/index.js
+++ aulas/aul4/index.js
@@ -1,1 +1,14 @@
-print("TEste")
+
+const Pessoa = {
+    nome: "Alek",
+    sobrenome: "Max",
+    altura: 1.67,
+    idade: 19,
+
+    envelhecer: function(anos){
+        return this.idade += anos
+    }
+};
+
+console.log(typeof(Pessoa));
+console.log(Pessoa.envelhecer(3));
