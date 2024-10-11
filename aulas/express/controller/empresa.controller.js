e33006bdfdfa7c0f234f8c0c7b909867265c67c3
Alekssandher <alekssandher1@hotmail.com>
2024-10-11 10:31:39 -0300

    update API

--- aulas/express/controller/empresa.controller.js
+++ aulas/express/controller/empresa.controller.js
@@ -24,20 +24,48 @@ const findAllEmpresas = (req, res) => {
 
 const find = (req, res) => {
     const id = req.params.id;
-    res.send(empresas[id]);
+
+    let found = false;
+
+    empresas.map(function(valor){
+        
+        if(valor.id == id){
+            found = true;
+            return res.send(valor);
+            
+        }
+    });
+    if (!found){
+        res.status(404).send({message: "Not found"});
+    }
+    
 };
 
 const createEmpresa = (req, res) => {
 
     const empresa = req.body;
 
-    if (req.body.nome == null) {
-        return res.send({message: "Corpo da mensagem vazio!"});
+    if (Object.keys(empresa).length === 0) {
+        return res.status(400).send({message: "Corpo da mensagem vazio!"});
     };
 
+    if (!empresa.id){
+        return res.status(400).send({message: "Campo ID não encontrado"});
+    };
+
+    if (!empresa.nome){
+        return res.status(400).send({message: "Campo nome não encontrado"});
+    };
+
+    if (!empresa.numFuncionarios){
+        return res.status(400).send({message: "Campo numFuncionarios não encontrado"});
+    };
+
+    empresa.nacionalidade = "BR";
+    
     empresas.push(empresa);
 
-    res.send(empresas);
+    res.status(201).send(empresas);
 
 };
 
